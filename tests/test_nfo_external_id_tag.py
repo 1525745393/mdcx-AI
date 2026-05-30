d8b4776012476882515917e20c9d35754a6a0e79
1525745393 <1525745393@users.noreply.github.com>
2026-05-30 15:16:18 +0000

    test: improve get_external_id_tag_name tests

--- tests/test_nfo_external_id_tag.py
+++ tests/test_nfo_external_id_tag.py
@@ -1,10 +1,24 @@
+import pytest
+
 from mdcx.config.enums import Website
 from mdcx.core.nfo import get_external_id_tag_name
 
 
-def test_get_external_id_tag_name_for_normal_site():
-    assert get_external_id_tag_name(Website.JAVDB) == "javdbid"
-
-
-def test_get_external_id_tag_name_for_site_starting_with_digit():
-    assert get_external_id_tag_name(Website.MMTV) == "mmtvid"
+@pytest.mark.parametrize(
+    "input_site, expected_tag_name",
+    [
+        (Website.JAVBUS, "javbusid"),
+        (Website.JAVDB, "javdbid"),
+        (Website.DMM, "dmmid"),
+        (Website.MMTV, "mmtvid"),
+        ("123SITE", "SITEid"),
+        ("abc123Site", "Siteid"),
+        ("ABC", "ABCid"),
+        ("", "siteid"),
+        (None, "siteid"),
+    ],
+)
+def test_get_external_id_tag_name(input_site, expected_tag_name):
+    """Test that get_external_id_tag_name correctly formats external id tags"""
+    result = get_external_id_tag_name(input_site)
+    assert result == expected_tag_name
