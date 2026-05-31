73234c76c44767f3e490d8214c99a617307e638f
1525745393 <1525745393@users.noreply.github.com>
2026-05-31 06:38:32 +0000

    release: v220260564 版本发布

--- mdcx/consts.py
+++ mdcx/consts.py
@@ -2,7 +2,7 @@ import platform
 import sys
 from pathlib import Path
 
-LOCAL_VERSION = 220260563
+LOCAL_VERSION = 220260564
 
 GITHUB_REPO = "1525745393/mdcx-AI"
 GITHUB_RELEASES_URL = f"https://github.com/{GITHUB_REPO}/releases"
